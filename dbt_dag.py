6fe9e3384e1496acbeedaf6ab8450844a64b1daf
Angel Alberto Márquez Vargas <angelalberto.marquez@tui.com>
2022-01-01 11:26:08 +0100

    Update dbt_dag.py

--- dbt_dag.py
+++ dbt_dag.py
@@ -1,5 +1,4 @@
-import datetime
-import json
+from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -7,69 +6,21 @@ from airflow.utils.dates import datetime
 from airflow.utils.dates import timedelta
 
 
-dag = DAG(
+with DAG(
     'dbt_dag',
-    start_date=datetime(2020, 12, 23),
-    description='A dbt wrapper for airflow',
+    start_date=datetime(2021, 12, 23),
+    description='An Airflow DAG to invoke simple dbt commands',
     schedule_interval=timedelta(days=1),
-)
+) as dag:
 
-def load_manifest():
-    local_filepath = "/usr/local/airflow/dags/dbt/target/manifest.json"
-    with open(local_filepath) as f:
-        data = json.load(f)
+    dbt_run = BashOperator(
+        task_id='dbt_run',
+        bash_command='dbt run'
+    )
 
-    return data
+    dbt_test = BashOperator(
+        task_id='dbt_test',
+        bash_command='dbt test'
+    )
 
-def make_dbt_task(node, dbt_verb):
-    """Returns an Airflow operator either run and test an individual model"""
-    DBT_DIR = "/usr/local/airflow/dags/dbt"
-    GLOBAL_CLI_FLAGS = "--no-write-json"
-    model = node.split(".")[-1]
-
-    if dbt_verb == "run":
-        dbt_task = BashOperator(
-            task_id=node,
-            bash_command=f"""
-            cd {DBT_DIR} &&
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target prod --models {model}
-            """,
-            dag=dag,
-        )
-
-    elif dbt_verb == "test":
-        node_test = node.replace("model", "test")
-        dbt_task = BashOperator(
-            task_id=node_test,
-            bash_command=f"""
-            cd {DBT_DIR} &&
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target prod --models {model}
-            """,
-            dag=dag,
-        )
-
-    return dbt_task
-
-data = load_manifest()
-
-dbt_tasks = {}
-for node in data["nodes"].keys():
-    if node.split(".")[0] == "model":
-        node_test = node.replace("model", "test")
-
-        dbt_tasks[node] = make_dbt_task(node, "run")
-        dbt_tasks[node_test] = make_dbt_task(node, "test")
-
-for node in data["nodes"].keys():
-    if node.split(".")[0] == "model":
-
-        # Set dependency to run tests on a model after model runs finishes
-        node_test = node.replace("model", "test")
-        dbt_tasks[node] >> dbt_tasks[node_test]
-
-        # Set all model -> model dependencies
-        for upstream_node in data["nodes"][node]["depends_on"]["nodes"]:
-
-            upstream_node_type = upstream_node.split(".")[0]
-            if upstream_node_type == "model":
-                dbt_tasks[upstream_node] >> dbt_tasks[node]
+    dbt_run >> dbt_test
